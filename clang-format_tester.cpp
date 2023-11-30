a1251c67fa325320aeec0e985e0caefe0f7bc460
Nicolas Boutin <nicolas.boutin@external.magnetimarelli.com>
2023-11-30 11:40:32 +0100

    AlwaysBreakAfterReturnType: None

--- clang-format_tester.cpp
+++ clang-format_tester.cpp
@@ -230,3 +230,22 @@ int main()
 
   return 0;
 }
+
+// AlwaysBreakAfterReturnType
+uint8_t CRCC_CalculateCRC8_hook(const uint8* data,
+                                uint32 size,
+                                Crcc_ConfigId_t cfgId,
+                                bool_t isFirstCall,
+                                uint8* result)
+{
+  uint8_t start_value = 0x00;
+  (void) cfgId;
+
+  if (isFirstCall == FALSE)
+  {
+    start_value = *result;
+  }
+
+  *result = CRC8_SAE_J1850_Calculate(start_value, data, size);
+  return CRCC_OK;
+}
